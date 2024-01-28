825f6b09dc7673b48afe419724ae90fcb982fff9
mmtszk <mikolaj.matuszak@protonmail.com>
2024-01-28 11:33:44 +0100

    Wprowadzanie znakow

--- kalkulator.py
+++ kalkulator.py
@@ -12,3 +12,11 @@ def podziel(x, y):
        return x / y
    else:
        return 'Błąd: Dzielenie przez zero jest niedozwolone'
+
+   print("Wybierz operację.")
+   print("1.Dodaj")
+   print("2.Odejmij")
+   print("3.Pomnóż")
+   print("4.Podziel")
+
+   wybor = input("Podaj wybór(1/2/3/4):")
