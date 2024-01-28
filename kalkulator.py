4c3c39f2e2ebb910e0f0311fa57627499ff34489
mmtszk <mikolaj.matuszak@protonmail.com>
2024-01-28 11:30:41 +0100

    Mnozenie i dzielenie

--- kalkulator.py
+++ kalkulator.py
@@ -3,3 +3,12 @@ def dodaj(x, y):
 
 def odejmij(x, y):
    return x - y
+
+def pomnoz(x, y):
+   return x * y
+
+def podziel(x, y):
+   if y != 0:
+       return x / y
+   else:
+       return 'Błąd: Dzielenie przez zero jest niedozwolone'
